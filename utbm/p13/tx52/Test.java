8e350b547bed6e3a0b2e141a02e03361ce565076
briardg <briardg@gmail.com>
2013-11-16 14:59:54 +0100

    test gaut 2

--- utbm/p13/tx52/Test.java
+++ utbm/p13/tx52/Test.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package utbm.p13.tx52;
 
 import utbm.p13.tx52.connection.ClientSocketReceiver;
